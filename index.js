9c09249d5737f5d58e0299ba8e598e1926e5a4da
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-09-13 17:23:48 +0530

    added timing

--- index.js
+++ index.js
@@ -325,12 +325,8 @@ const buttonId1Response= (phone_number_id, from) => {
   })
   // for(let i=0;i<foodMenu.length;i++){
     axios({
-      method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-      url:
-        "https://graph.facebook.com/v12.0/" +
-        phone_number_id +
-        "/messages?access_token=" +
-        token,
+      method: "POST",
+      url: `https://graph.facebook.com/v12.0/${phone_number_id}/messages?access_token=${token}`,
       data: {
         messaging_product: "whatsapp",
         recipient_type: "individual",
@@ -338,10 +334,29 @@ const buttonId1Response= (phone_number_id, from) => {
         type: "document",
         document: {
           link: "https://drive.google.com/file/d/17GAz0kkUuEp_RXhtKLMee0ttIGZLGihz/view?usp=sharing.pdf",
+          content_type: "application/pdf" // Specify the content type as PDF
         }
       },
       headers: { "Content-Type": "application/json" },
-    })
+    });
+    // axios({
+    //   method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+    //   url:
+    //     "https://graph.facebook.com/v12.0/" +
+    //     phone_number_id +
+    //     "/messages?access_token=" +
+    //     token,
+    //   data: {
+    //     messaging_product: "whatsapp",
+    //     recipient_type: "individual",
+    //     to: from,
+    //     type: "document",
+    //     document: {
+    //       link: "https://drive.google.com/file/d/17GAz0kkUuEp_RXhtKLMee0ttIGZLGihz/view?usp=sharing.pdf",
+    //     }
+    //   },
+    //   headers: { "Content-Type": "application/json" },
+    // })
   // }
 }
 
