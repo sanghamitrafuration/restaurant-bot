762ad18ec97301e50586216eaa36502b0479b7e1
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-09-14 12:42:21 +0530

    added instagram link

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const axios = require("axios").default;
 const app = express().use(body_parser.json()); // creates express http server
 const PORT = process.env.PORT || 1337;
 
+import Food from "./images/food-1.pdf";
+
 const foodMenu = [
   "https://i.postimg.cc/jS1xkHFQ/food-1.png",
   "https://i.postimg.cc/nc5gy3nz/food-2.png",
@@ -425,11 +427,12 @@ const buttonId1Response = (phone_number_id, from) => {
       messaging_product: "whatsapp",
       recipient_type: "individual",
       to: from,
-      type: "document",
-      document: {
-        link: pdfLink,
-        mime_type: "application/pdf",
-      },
+      "type": "document",
+      "document": {
+          "id": "FOOD_MENU",
+          "caption": "<Food menu",
+          "filename": Food
+      }
     },
     headers: { "Content-Type": "application/json" },
   });
